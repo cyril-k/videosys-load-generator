b39b0712683234eb8795cac084efb49d7256655a
Cyril Kondratenko <kkn1993@gmail.com>
2024-11-06 10:16:15 +0000

    add spec configmap and upload result

--- app/app.py
+++ app/app.py
@@ -9,6 +9,7 @@ from prometheus_client import make_asgi_app
 
 from metrics import Metrics
 from on_startup import run_benchmark
+from utils import sanitize_label
 
 logging.basicConfig(
     level=logging.INFO,
@@ -27,12 +28,6 @@ metrics = Metrics(
 
 app = FastAPI()
 
-def sanitize_label(label_name: str) -> str:
-    sanitized_label = re.sub(r'[^a-zA-Z0-9_]', '_', label_name)
-    if sanitized_label[0].isdigit():
-        sanitized_label = f"_{sanitized_label}"
-
-    return sanitized_label
 
 async def benchmark_task():
     global metrics
--- app/on_startup.py
+++ app/on_startup.py
@@ -4,8 +4,14 @@ import copy
 import logging 
 import requests
 import aiohttp
+from datetime import datetime
 
 from metrics import Metrics
+from utils import (
+    save_result,
+    get_instance_name,
+    get_specs
+)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -18,6 +24,11 @@ logger = logging.getLogger(__name__)
 SERVER_URL = "http://localhost:8000"
 RETRY_INTERVAL = 5
 AIOHTTP_TIMEOUT = aiohttp.ClientTimeout(total=6 * 60 * 60)
+SPEC = {
+    "served_model": os.environ.get("SERVED_MODEL", "THUDM/CogVideoX-5b"),
+    "batch_size_list": list(range(1, 9)),
+    "num_inference_steps_list": list(range(20, 110, 10)),
+}
 
 def check_videosys_health():
     while True:
@@ -35,16 +46,20 @@ def check_videosys_health():
 
 
 async def run_benchmark(metrics: Metrics):
+    spec = get_specs() or SPEC
+    served_model = spec.get("served_model")
+    instance_name = get_instance_name()
+    timestamp = datetime.utcnow().replace(microsecond=0).isoformat() + 'Z'
+    result_file_name = f"videosys_{metrics.labels['model']}_{instance_name}_{timestamp}"
+    results = []
 
     check_videosys_health()
 
     api_url = f"{SERVER_URL}/generate"
     start_time = time.perf_counter()
     async with aiohttp.ClientSession(timeout=AIOHTTP_TIMEOUT) as session:
-
-
-        for batch_size in range(1, 9):
-            for num_steps in range (20, 110, 10):
+        for batch_size in spec.get("batch_size_list"):
+            for num_steps in spec.get("num_inference_steps_list"):
                 payload = {
                     "prompt": [
                         "Sunset over the sea.",
@@ -64,7 +79,6 @@ async def run_benchmark(metrics: Metrics):
                     async with session.post(url=api_url, json=payload,
                                     headers=headers) as response:
                         response.raise_for_status()
-                        print(await response.json())
                         inference_time = float((await response.json())["elapsed_time"])
                         histogram_labels = copy.deepcopy(metrics.labels)
                         histogram_labels["bs"] = batch_size
@@ -78,9 +92,28 @@ async def run_benchmark(metrics: Metrics):
                 except Exception as e:
                     logger.exception(e)
                     metrics.fail_count.labels(**metrics.labels).inc()
+                    inference_time = None
                 finally:
                     metrics.total_gen_count.labels(**metrics.labels).inc()
                     metrics.total_steps.labels(**metrics.labels).inc(num_steps)
                     metrics.total_outputs.labels(**metrics.labels).inc(batch_size)
+                    results.append(
+                        {   
+                            "served_model": served_model,
+                            "batch_size": batch_size,
+                            "num_inference_steps": num_steps,
+                            "inference_time": inference_time,
+                            "payload": payload
+                        }
+                    )
     end_time = time.perf_counter()
-    metrics.bench_duration.labels(**metrics.labels).set(end_time - start_time)
+    benchmark_duration = end_time - start_time
+    metrics.bench_duration.labels(**metrics.labels).set(benchmark_duration)
+    save_result(
+        result_dict={
+            "instance_name": instance_name,
+            "bencmark_duration": benchmark_duration,
+            "results": results
+        },
+        file_name=result_file_name,
+    )
